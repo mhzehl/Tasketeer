d777ac475fbc1bbda796af0aeff4c4791f17c843
Melvin <melvin.zehl@s5.nl>
2018-01-09 20:37:17 +0100

    Add project files.

---
+++ Tasketeer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tasketeer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
