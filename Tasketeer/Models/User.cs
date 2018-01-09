1a2edc8cdbd70d035042b6ce64e636f6ac5b76c4
Melvin <melvin.zehl@s5.nl>
2018-01-09 21:05:39 +0100

    Add Todo and User models

---
+++ Tasketeer/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tasketeer.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Prefix { get; set; }
+        public string Lastname { get; set; }
+    }
+}
