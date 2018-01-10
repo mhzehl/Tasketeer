5d3743faad98eee461c06da9a4ffeb33637da6ea
Melvin <melvin.zehl@s5.nl>
2018-01-10 01:12:21 +0100

    Add first controller actions (GET)

---
+++ Tasketeer/Controllers/TodosController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tasketeer.Models;
+
+namespace Tasketeer.Controllers
+{
+    [Route("/")]
+    public class TodosController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public TodosController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var todos = await _context.Todos
+                .ToListAsync();
+
+            return Ok(todos);
+        }
+
+    }
+}
---
+++ Tasketeer/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tasketeer.Models;
+
+namespace Tasketeer.Controllers
+{
+    [Route("/users")]
+    public class UsersController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public UsersController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var users = await _context.Users
+                .ToListAsync();
+
+            return Ok(users);
+        }
+
+    }
+}
